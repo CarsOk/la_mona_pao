2de5726579dd4ca7b33937db838ad8a794008119
Fabiodelahoz <91040882+Orla08@users.noreply.github.com>
2022-06-06 11:04:05 -0500

    Realizo el script del proyecto

--- BDD/Scripts/script_Proyecto.sql
+++ BDD/Scripts/script_Proyecto.sql
@@ -5,28 +5,20 @@ show databases;
 use baseDeDatos;
 
 create table Pedido(Cod_Pedido integer, Usuario integer, Cod_Menu integer, Cod_plato integer,
-total integer , primary key(Usuario)   );
+total integer , primary key(Cod_Pedido)   );
+
+create table Pedido(Cod_Detalle integer, Cod_pedido integer, Cod_Plato integer, Cantidad integer,
+Precio_Unidad integer , SubTotal integer ,primary key(Cod_Detalle)   );
 
 create table Usuario(Type_Doc varchar(2), Number_Doc integer, Nombre varchar(26), Last_Name varchar(26),
 Years_Old integer, Direccion varchar(20), Email varchar(30), Contrasena varchar(12),
 primary key(Number_Doc)    );
 
-create table Tipo_De_Comida( Codigo integer, Tipos_De_Comida varchar(25),
-primary key(Codigo)    );
+create table Menu (Cod_Menu integer, Tipo_De_Comida varchar(25),
+descr_plato varchar(300), primary key(Codigo_Plato)    );
 
-create table Plataos(Codigo integer, Cod_TipDeCom integer,
-plato varchar(25), precio integer,
-primary key(Codigo)     );
-
-create table Sabor_bebida(Codigo integer, Sabor_bebida varchar(20),
-primary key(Codigo)     );
-
-create table Cant_Bebida(Codigo integer, cod_Sabor_Bebida integer,
-cantidad varchar(10), precio integer,
-primary key(Codigo)     );
-
-create table Adicionales(Codigo integer, Adicional varchar(16), precio integer,
-primary key(Codigo)     );
+create table Platos(Cod_Plato integer, Cod_Menu integer, Plato varchar(20), 
+descr_plato varchar(300), Precio integer, primary key(Codigo_Plato)    );
 
 create table insumos(Codigo integer, Producto varchar(20), cantidades varchar(15),
 primary key(Codigo)     );

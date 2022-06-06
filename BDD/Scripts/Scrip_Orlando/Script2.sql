d12c26b81552990af6a306bc9b7afc9c58552e67
Fabiodelahoz <91040882+Orla08@users.noreply.github.com>
2022-06-06 10:31:12 -0500

    Editando

--- BDD/Scripts/Scrip_Orlando/Script2.sql
+++ BDD/Scripts/Scrip_Orlando/Script2.sql
@@ -15,7 +15,7 @@ create table Usuario(Type_Doc varchar(2), Number_Doc integer, Nombre varchar(26)
 Years_Old integer, Direccion varchar(20), Email varchar(30), Contrasena varchar(12),
 primary key(Number_Doc)    );
 
-create table Menu( Codigo integer, Tipos_De_Comida varchar(25),
+create table Menu( Codigo integer, Tipos_De_Comida varchar(25), Descripcion Varchar(200),
 primary key(Codigo)    );
 
 create table Plataos(Codigo integer, Cod_TipDeCom integer,

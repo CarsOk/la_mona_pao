ffa0199eff9723188028dd004b80b9ab5269e0a0
Fabiodelahoz <91040882+Orla08@users.noreply.github.com>
2022-06-06 11:30:15 -0500

    Terminando scrpit

--- BDD/Scripts/script_Proyecto.sql
+++ BDD/Scripts/script_Proyecto.sql
@@ -7,7 +7,7 @@ use baseDeDatos;
 create table Pedido(Cod_Pedido integer, Usuario integer, Cod_Menu integer, Cod_plato integer,
 total integer , primary key(Cod_Pedido)   );
 
-create table Pedido(Cod_Detalle integer, Cod_pedido integer, Cod_Plato integer, Cantidad integer,
+create table Des_Pedido(Cod_Detalle integer, Cod_pedido integer, Cod_Plato integer, Cantidad integer,
 Precio_Unidad integer , SubTotal integer ,primary key(Cod_Detalle)   );
 
 create table Usuario(Type_Doc varchar(2), Number_Doc integer, Nombre varchar(26), Last_Name varchar(26),

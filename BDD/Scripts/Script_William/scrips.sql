bc8300c68bbd405c7f44806bfa4cbfb57fcc1a5b
shelcyhernandez <99417349+williamcortes17@users.noreply.github.com>
2022-06-09 11:41:15 -0500

    ingrese actas

--- BDD/Scripts/Script_William/scrips.sql
+++ BDD/Scripts/Script_William/scrips.sql
@@ -1,6 +1,6 @@
 mysql-u root -p 
 password:
-select title from film where rating="pg";
-update film set title="operacion estrella" description "vaca loca" where film-id =841;
-select film title, language. name from film, language where film.language - id = language. language - id;
-update film set language - id = 4 where film - id =6;
+SELECT title from film where rating="pg";
+update film set title = "OPERACION ESTRELLA" description "vaca loca" where film - id = 841;
+SELECT film. title, language.name from film, language where film.language - id = language. language - id;
+update film set language - id = 4 where film -id =6; 
